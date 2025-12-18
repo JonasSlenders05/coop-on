706f7f5c5ac399b672e0bc5267bc502d48483310
JonasSlenders05 <jonas.slenders@student.hogent.be>
2025-12-18 14:11:07 +0100

    Fix?

--- webservices-jeton/src/app.controller.ts
+++ webservices-jeton/src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Coop-on API is running! 🚀 Ga naar /api/docs voor documentatie.';
   }
 }
